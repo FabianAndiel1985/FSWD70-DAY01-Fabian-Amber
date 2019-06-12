0a0898c11ebb18997de2391f63d2d8d1044abb2e
Fabian Andiel <fabian.andiel@yahoo.com>
2019-06-12 10:29:03 +0200

    Basic ex sets

---
+++ multiplication.js
@@ -0,0 +1,5 @@
+for (var y = 1; y < 11; y++) {
+    for (var x = 1; x < 11; x++) {
+        console.log(y + "*" + x + "=" + x * y + "<br>");
+    }
+}
---
+++ multiplication.ts
@@ -0,0 +1,6 @@
+for (var y=1; y<11;y++)
+{
+	for (var x=1; x< 11; x++) {
+	console.log(y + "*" + x +"="+ x*y );
+	}
+}
